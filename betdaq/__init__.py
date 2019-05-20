2a87ab6997e6573d92782fb4ebb74d39f5411fb9
Rory Cole <rory.cole1990@gmail.com>
2019-05-20 21:38:08 +0100

    Remove imports from init

--- betdaq/__init__.py
+++ betdaq/__init__.py
@@ -1,7 +1,2 @@
-from .apiclient import APIClient
-from .exceptions import BetdaqError
-from .filters import create_order, update_order
-
-
 __title__ = 'betdaq'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
